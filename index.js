89156dd847792c814030db1b8d463f5de941cb89
Anh Ngo <adn057@ucsd.edu>
2018-02-03 10:53:37 -0800

    implement button clicking

--- index.js
+++ index.js
@@ -14,40 +14,107 @@ function init() {
 	ctx.fillStyle = "#6ec6ff";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-	//DRAWS IMAGES====================
-	var heartImg = new Image();
-	heartImg.src = "./images/ic_favorite_black_24px.svg";
-	heartImg.onload = function() {
-		ctx.drawImage(heartImg, margin, margin, iconSize, iconSize);
-	}
+	//DRAWS ICONS====================
+	//stores images in array to check for clicks
+	var images = [];
 
-	var foodImg = new Image();
-	foodImg.src = "./images/ic_restaurant_black_18px.svg";
-	foodImg.onload = function() {
-		ctx.drawImage(foodImg, margin, canvas.height - margin - iconSize, iconSize, iconSize);
+	//top left images
+	var img0 = new Image();
+	img0.name = "heart";
+	img0.location = {
+		x: margin, 
+		y: margin
+	};
+	img0.src = "./images/ic_favorite_black_24px.svg";
+	img0.onload = function() {
+		ctx.drawImage(img0, img0.location.x, img0.location.y, iconSize, iconSize);
 	}
+	images.push(img0);
 
-	var starImg = new Image();
-	starImg.src = "./images/ic_star_rate_black_18px.svg";
-	starImg.onload = function() {
-		ctx.drawImage(starImg,canvas.width - margin - iconSize, margin, iconSize, iconSize);
+	//bottm left image
+	var img1 = new Image();
+	img1.name = "food";
+	img1.location = {
+		x: margin, 
+		y: canvas.height - margin - iconSize
+	};
+	img1.src = "./images/ic_restaurant_black_18px.svg";
+	img1.onload = function() {
+		ctx.drawImage(img1, img1.location.x, img1.location.y, iconSize, iconSize);
 	}
+	images.push(img1);
 
-	var warnImg = new Image();
-	warnImg.src = "./images/ic_warning_black_18px.svg";
-	warnImg.onload = function() {
-		ctx.drawImage(warnImg,canvas.width - margin - iconSize,canvas.height - margin - iconSize, iconSize, iconSize);
+	//top right image
+	var img2 = new Image();
+	img2.name = "star";
+	img2.location = {
+		x: canvas.width - margin - iconSize, 
+		y: margin
+	};img2.src = "./images/ic_star_rate_black_18px.svg";
+	img2.onload = function() {
+		ctx.drawImage(img2, img2.location.x, img2.location.y, iconSize, iconSize);
 	}
+	images.push(img2);
+
+	//bottom right image
+	var img3 = new Image();
+	img3.name = "warning";
+	img3.location = {
+		x: canvas.width - margin - iconSize, 
+		y: canvas.height - margin - iconSize
+	};img3.src = "./images/ic_warning_black_18px.svg";
+	img3.onload = function() {
+		ctx.drawImage(img3, img3.location.x, img3.location.y, iconSize, iconSize);
+	}
+	images.push(img3);
+
 	//=================================
 
-/*
-	ctx.addHitRegion({id: "canvas"});
-	canvas.onclick = function(event) {
-		if(event.region){
-			console.log(event.region);
-		}
+	//Handles clicks
+	$('#canvas').click(function(e){
+	    var x = e.clientX
+		  , y = e.clientY;
+
+		console.log(x);
+		console.log(y);
+
+		//handle which button was pressed
+		images.forEach(function(element) {
+			if(e.clientX > element.location.x &&
+			   e.clientX < element.location.x + iconSize &&
+			   e.clientY > element.location.y &&
+			   e.clientY < element.location.y + iconSize){
+
+				element.onClick();
+			}else{
+				onClick();
+			}
+		});
+	})  
+
+
+	//things that happen on click
+	img0.onClick = function() {
+		console.log("Button 0 Clicked");
 	}
-	*/
+
+	img1.onClick = function() {
+		console.log("Button 1 Clicked");
+	}
+
+	img2.onClick = function() {
+		console.log("Button 2 Clicked");
+	}
+
+	img3.onClick = function() {
+		console.log("Button 3 Clicked");
+	}
+
+	function onClick() {
+		//console.log("No Button Clicked");
+	}
 }
 
+var draw = function() {
 
+}
