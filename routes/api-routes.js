1bf3ae1244e72d8537e0626aa537325324c98b37
Clark <clarkphan@github.com>
2018-02-03 01:05:25 -0800

    install sendgrid api

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,11 +1,32 @@
-// Set up express 
+// configure environment variables in .env
+require('dotenv').config()
+
+// sendgrid api dependency
+var sgMail = require('@sendgrid/mail');
+
+// configure api key for access
+sgMail.setApiKey(process.env.api_key);
+
+// set up express 
 var express = require("express");
 
-// Sets up a router to handle client requests
+// sets up a router to handle client requests
 var router = express.Router();
 
-router.get("/", function (req, res) {
-  res.send("Wassup HACKUCI!");
+router.get("/send/mail", function (req, res) {
+  // set up email message object
+  var msg = {
+    to: 'clarkphan24@gmail.com',
+    from: 'test@example.com',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+
+  // send mail
+  sgMail.send(msg);
+  
+  res.send("message sent");
 });
 
 module.exports = router;
