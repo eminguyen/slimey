3ab3109866e0282a3488460d7160518401fd7a51
Anh Ngo <adn057@ucsd.edu>
2018-02-03 01:06:01 -0800

    turn icon sizes to variables

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function init() {
+
+	var iconSize = 50;
+	var margin = 10;
 	
 	var canvas = document.getElementById("canvas");
 	var ctx = canvas.getContext("2d");
@@ -15,34 +18,36 @@ function init() {
 	var heartImg = new Image();
 	heartImg.src = "./images/ic_favorite_black_24px.svg";
 	heartImg.onload = function() {
-		ctx.drawImage(heartImg,10,10, 50, 50);
+		ctx.drawImage(heartImg, margin, margin, iconSize, iconSize);
 	}
 
 	var foodImg = new Image();
 	foodImg.src = "./images/ic_restaurant_black_18px.svg";
 	foodImg.onload = function() {
-		ctx.drawImage(foodImg,10,canvas.height - 60, 50, 50);
+		ctx.drawImage(foodImg, margin, canvas.height - margin - iconSize, iconSize, iconSize);
 	}
 
 	var starImg = new Image();
 	starImg.src = "./images/ic_star_rate_black_18px.svg";
 	starImg.onload = function() {
-		ctx.drawImage(starImg,canvas.width - 60,10, 50, 50);
+		ctx.drawImage(starImg,canvas.width - margin - iconSize, margin, iconSize, iconSize);
 	}
 
 	var warnImg = new Image();
 	warnImg.src = "./images/ic_warning_black_18px.svg";
 	warnImg.onload = function() {
-		ctx.drawImage(warnImg,canvas.width - 60,canvas.height - 60, 50, 50);
+		ctx.drawImage(warnImg,canvas.width - margin - iconSize,canvas.height - margin - iconSize, iconSize, iconSize);
 	}
 	//=================================
 
+/*
 	ctx.addHitRegion({id: "canvas"});
 	canvas.onclick = function(event) {
 		if(event.region){
 			console.log(event.region);
 		}
 	}
+	*/
 }
 
 
